12af667b89fc2df21ee941c972b829e3a43658b3
Danil Shulgin <nik.vasilevich.01@gmail.com>
2021-05-22 13:25:06 +0300

    add sudo command for bot.sh

--- bot.sh
+++ bot.sh
@@ -1,2 +1,2 @@
-apt-get update
+sudo apt-get update
 mvn clean install && cd hello_bot && mvn exec:java -Dexec.mainClass="kpi.acts.appz.bot.hellobot.HelloWorldBot" -Dexec.args="1683870246:AAGDPr_vEEHxU686kyep2k79Derf2Y-Jo9M dummybot"
