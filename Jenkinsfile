2367076d2471252264d8a09080a58a6218b66a99
Danil Shulgin <nik.vasilevich.01@gmail.com>
2021-05-22 13:12:28 +0300

    try start bot

--- Jenkinsfile
+++ Jenkinsfile
@@ -5,6 +5,7 @@ pipeline {
         stage('Build') {
             steps {
                 echo 'Building..'
+                sh "./bot.sh"
             }
         }
         stage('Test') {
---
+++ bot.sh
@@ -0,0 +1,1 @@
+mvn clean install && cd hello_bot && mvn exec:java -Dexec.mainClass="kpi.acts.appz.bot.hellobot.HelloWorldBot" -Dexec.args="1683870246:AAGDPr_vEEHxU686kyep2k79Derf2Y-Jo9M dummybot"
