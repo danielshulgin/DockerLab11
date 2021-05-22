208e5a1339d9697235985883fa22daf1780ad0b0
Danil Shulgin <nik.vasilevich.01@gmail.com>
2021-05-22 14:45:57 +0300

    remove commands from bot sh

--- bot.sh
+++ bot.sh
@@ -1,2 +1,2 @@
-sudo apt-get update
-mvn clean install && cd hello_bot && mvn exec:java -Dexec.mainClass="kpi.acts.appz.bot.hellobot.HelloWorldBot" -Dexec.args="1683870246:AAGDPr_vEEHxU686kyep2k79Derf2Y-Jo9M dummybot"
+#udo apt-get update
+#mvn clean install && cd hello_bot && mvn exec:java -Dexec.mainClass="kpi.acts.appz.bot.hellobot.HelloWorldBot" -Dexec.args="1683870246:AAGDPr_vEEHxU686kyep2k79Derf2Y-Jo9M dummybot"
